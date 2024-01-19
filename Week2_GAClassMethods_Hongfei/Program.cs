5f44e53fe29693fda3a46bd7722dbfe96015af7a
Lisa-cai <145958405+Lisa-cai@users.noreply.github.com>
2024-01-19 00:43:57 -0800

    Add project files.

---
+++ Week2_GAClassMethods_Hongfei/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_GAClassMethods_Hongfei
+{
+    //Hongfei
+    //Week2_GAClassMethods
+    //01/18/2024
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> samGrades = new List<int> { 95, 87, 88, 90, 75 };
+            Student samStudent = new Student("Sam", samGrades);
+
+            //test AddGrade method
+            samStudent.AddGrade(70);
+            samStudent.AddGrade(125);
+            samStudent.AddGrade(-98);
+
+            samStudent.DisplayAllGrades();
+            samStudent.GetGrade();
+            samStudent.DiplayStudentInfo();
+
+            //Generage random students and add students list
+            static List<Student> GenerateRandomStudents()
+            {
+                List<Student> students = new List<Student>();
+                //Random object created
+                Random random = new Random();
+                for( int i=1; i <=5; i++ )
+                {
+                    //Create new instance with student name value
+                    Student student = new Student($"Student{i}");
+                    for ( int j = 0; j<5; j++ ) 
+                    { 
+                        //Add random score into student score list
+                        student.ExamScoures.Add(random.Next(0,101));
+                    }
+                    //Each of student with 5 exam scores add students list
+                    students.Add(student);
+                }
+                return students;
+            }
+
+            List<Student> randomStudents = GenerateRandomStudents();
+            //Add student to this list 
+            randomStudents.Add(samStudent);
+
+            //Loop and display all the info
+            foreach(Student student in randomStudents) 
+            {
+                student.DiplayStudentInfo();
+            }
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
---
+++ Week2_GAClassMethods_Hongfei/Student.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_GAClassMethods_Hongfei
+{
+    //field
+    public class Student
+    {
+        public string name;
+        public List<int> examScoures = new List<int>();
+
+        //properties 
+        public string Name { get => name; set => name = value; }
+        public List<int> ExamScoures { get => examScoures; set => examScoures=value; }
+
+        public Student(string name)
+        {
+            Name=name;
+            ExamScoures = new List<int>();
+        }
+
+        public Student(string name,List<int> examScoures)
+        {
+            Name=name;
+            ExamScoures=examScoures;
+        }
+
+        //Method 1 for addding list of numerical grades score on the ExamScores list
+        public void AddGrade(int grade)
+        {
+            //Add validation, only allow 0-100 scores can add to the list
+            if (grade >0 && grade <= 100)
+            {
+                //validation input sorce then add grade to ExamSours list
+                ExamScoures.Add(grade);
+                Console.WriteLine($"Add grade {grade} for {Name}");
+            }
+            else
+            {
+                //Display the informaiton why the input score couldn't add to ExamScores list
+                Console.WriteLine($"Invalid grade : {grade}. Grade must be between 0 and 100.");
+            }
+        }
+
+        //Method 2 for grade letter based on the grade score
+        private char GetletterGrade(int score)
+        {
+            if (score>= 90)
+            {
+                return 'A';
+            }
+            else if (score >=80)
+            {
+                return 'B';
+            }
+            else if (score >=70)
+            {
+                return 'C';
+            }
+            else if (score >= 60)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
+        }
+
+        //Method 3: display all the grades and grade letter realted to the student
+        public void DisplayAllGrades()
+        {
+            //use loop to call the getting grades letter method to get each of grades letter
+            //And dispay the grades letter and grade
+            Console.WriteLine($" The grades for {Name}");
+            for( int i=0; i < ExamScoures.Count; i++ )
+            {
+                //Call method 2 get grade letter
+                char letterGrade = GetletterGrade(ExamScoures[i]);
+                //Display 
+                Console.WriteLine($" Exam {i+1} : {letterGrade} ({ExamScoures[i]})");
+            }
+
+        }
+
+        //Method 4 for Caculating average of all the exams scores
+        public double CaculateAverageScores() 
+        {
+            if (ExamScoures.Count == 0)
+            {
+                return 0;
+            }
+            int totalScore = 0;
+            foreach ( int score in ExamScoures )
+            {
+                totalScore+=score;
+            }
+            return (double)totalScore/ ExamScoures.Count;
+        }
+
+        //Method 5 based on the average grades to get overall letter grade
+        public char GetGrade()
+        {
+            //call method 4 get average score
+            double averageScore = CaculateAverageScores();
+            //Round the average score to the whole number
+            int roundedAverageScore = (int)Math.Round(averageScore);
+            //call method 2 to get the letter grade
+            char averageLetterGrade = GetletterGrade(roundedAverageScore);
+            Console.WriteLine($" The average score is {roundedAverageScore}, ({averageLetterGrade}).");
+            return (GetletterGrade(roundedAverageScore));
+        }
+
+        //Method 6 Display all information including student name, average score and letter grade
+        public void DiplayStudentInfo()
+        {
+            //Get the average score
+            double averageScore = CaculateAverageScores();
+            //Get the average letter grade
+            char grade = GetGrade();
+            //Display the student name
+            Console.WriteLine($"Student Name: {Name}");
+            //Display the average score
+            Console.WriteLine($"Average score: {averageScore:F2}");
+            //Display the final grade
+            Console.WriteLine($"Grade: {grade}");
+
+        }
+    }
+
+}
